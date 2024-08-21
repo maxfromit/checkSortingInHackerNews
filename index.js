6ea5d96a02c613d7895e54aeacc75a9e1ad7fc79
maximfromit <bigmaxcapacity@gmail.com>
2024-08-21 05:10:40 +0300

    index set headless to true for git

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const extractNewsData = async (page) => {
 }
 
 export const fetchAndSortHackerNewsArticles = async () => {
-  const browser = await chromium.launch({ headless: false })
+  const browser = await chromium.launch({ headless: true })
   const context = await browser.newContext()
   const page = await context.newPage()
   page.on("requestfailed", (request) => {
