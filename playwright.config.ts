8cefc67b332ae3aed9465ad2815c12a9a0ab60ef
maximfromit <bigmaxcapacity@gmail.com>
2024-08-21 05:02:52 +0300

    playwright config update set headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 30000,
   retries: 2,
   use: {
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: "retain-on-failure",
